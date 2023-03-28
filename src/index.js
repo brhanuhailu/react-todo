35211c8eebae4831d0f006f23b4a8bb9d671be0e
brhanuhailu <hailubrhanu29@gmail.com>
2023-03-28 05:30:06 +0100

    styleintrc

---
+++ src/index.js
@@ -0,0 +1,16 @@
+/* eslint-disable import/order */
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import TodoContainer from './functionBased/components/TodoContainer';
+import './style.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <TodoContainer />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
